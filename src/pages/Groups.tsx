1398eca18ec22355300498ba72c7ebe1d18a3ffa
boobaGreen <devboobagreen@gmail.com>
2024-06-25 00:28:19 +0200

    group v1 new 2

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Cookies from "js-cookie";
 import { useGetGroups } from "../hooks/useGetAllGroups";
