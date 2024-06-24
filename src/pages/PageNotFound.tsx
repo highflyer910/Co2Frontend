be0b56d6a913feeee0f82921520bc65466734b50
boobaGreen <devboobagreen@gmail.com>
2024-06-24 21:47:00 +0200

    first groups list 2

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -8,7 +8,7 @@ const PageNotFound: React.FC = () => {
       <h1 className="text-4xl font-bold text-green-700 mb-4">404</h1>
       <p className="text-xl text-green-900 mb-4">Page Not Found</p>
       <Link to="/" className="text-green-700 hover:text-green-900 underline">
-        Go back to Home
+        Go back to Home !
       </Link>
     </div>
   );
