d2ce7b184c76626d5f011d6f1eb60ce5e598ba19
boobaGreen <devboobagreen@gmail.com>
2024-06-25 22:16:43 +0200

    locatione for callback donate

--- src/pages/DonationCallback.tsx
+++ src/pages/DonationCallback.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const DonationCallback: React.FC = () => {
-  const { context, don_status } = useParams<{
-    context: string;
-    don_status: string;
-  }>();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // Parse query parameters
+  const queryParams = new URLSearchParams(location.search);
+  const context = queryParams.get("context");
+  const don_status = queryParams.get("don_status");
+
   console.log({ context });
   console.log({ don_status });
-  const navigate = useNavigate();
 
   const handleReturnHome = () => {
     navigate("/");
