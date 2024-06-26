0acb699da748fdfca7bd7406c795c37f3e8b4613
boobaGreen <devboobagreen@gmail.com>
2024-06-26 23:09:19 +0200

    group reload when tona da limit 2

--- src/pages/LimitPage.tsx
+++ src/pages/LimitPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Limit: React.FC = () => {
   const { groupId, groupName, groupLimits } = useParams<{
