1503df117e626379f3f7453233f3b63c6106b8ea
boobaGreen <devboobagreen@gmail.com>
2024-06-28 02:15:19 +0200

    indietro

--- src/pages/LimitPage.tsx
+++ src/pages/LimitPage.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useGetGroups } from "../hooks/useGetAllGroups"; // Assumi che il percorso sia corretto
 import { useMain } from "../contexts/MainContext";
-import { FaCrown } from "react-icons/fa";
 
 const Limit: React.FC = () => {
   const { groupId, groupName, groupLimits } = useParams<{
@@ -10,10 +9,6 @@ const Limit: React.FC = () => {
     groupName: string;
     groupLimits: string;
   }>();
-
-  const isAdmin = useParams<{ isAdmin: string }>().isAdmin === "true";
-
-  console.log("isAdmin:", isAdmin);
 
   const navigate = useNavigate();
   const { jwt } = useMain(); // Ottieni il token JWT
@@ -23,11 +18,11 @@ const Limit: React.FC = () => {
   const [limitValue, setLimitValue] = useState<number | null>(
     groupLimits && groupLimits !== "-1" ? +groupLimits : -1
   );
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [initialLimit, _] = useState<number>(
     groupLimits && groupLimits !== "-1" ? +groupLimits : -1
   );
+
   // State per gestire i messaggi di risposta
   const [responseMessage, setResponseMessage] = useState<string | null>(null);
 
@@ -39,7 +34,6 @@ const Limit: React.FC = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${jwt}`, // Aggiungi il JWT nell'header
           },
           body: JSON.stringify({
             chatId: groupId,
@@ -64,19 +58,17 @@ const Limit: React.FC = () => {
   const handleDeleteLimit = async () => {
     try {
       const response = await fetch(
-        `https://co2backend.onrender.com/api/v1/limit/generic/${groupId}`, // Includi l'ID del gruppo nel percorso URL
+        `https://co2backend.onrender.com/api/v1/limit/generic/${groupId}`,
         {
           method: "DELETE",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${jwt}`, // Aggiungi il JWT nell'header
           },
         }
       );
       const data = await response.json();
-      console.log("data quando cancello :", data);
+      setResponseMessage(data.success); // Mostra il messaggio di successo
 
-      setResponseMessage(data.success); // Mostra il messaggio di successo se presente
       setLimitValue(-1); // Resetta il valore del limite a -1 (valore di default)
     } catch (error) {
       console.error("Error deleting limit:", error);
@@ -93,51 +85,38 @@ const Limit: React.FC = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="font-poppins text-3xl font-bold text-center bg-yellow-200 text-green-800 py-3 px-4 shadow-lg">
         Limit Group Name: {groupName}
-        {isAdmin && (
-          <span className="ml-2 text-yellow-500">
-            <FaCrown />
-          </span>
-        )}
       </h1>
       <div className="my-4">
-        {isAdmin ? (
-          <div className="flex items-center mb-4">
-            <input
-              type="number"
-              className="rounded-l-lg p-2 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
-              placeholder="Enter limit value in KB"
-              value={
-                limitValue !== null && limitValue !== -1
-                  ? limitValue.toString()
-                  : ""
-              }
-              onChange={(e) => setLimitValue(Number(e.target.value))}
-            />
-            <button
-              onClick={handleSetLimit}
-              className={`px-4 p-2 rounded-r-lg text-white border border-r-0 ${
-                limitValue === initialLimit
-                  ? "bg-blue-200 cursor-not-allowed"
-                  : "bg-blue-500 hover:bg-blue-400"
-              }`}
-              disabled={limitValue === initialLimit} // Disabilita il pulsante se i limiti sono uguali
-            >
-              Set Limit
-            </button>
-          </div>
-        ) : (
-          <p className="font-bold text-red-600">
-            Only admins can set limits for this group.
-          </p>
-        )}
-        {isAdmin && (
+        <div className="flex items-center mb-4">
+          <input
+            type="number"
+            className="rounded-l-lg p-2 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
+            placeholder="Enter limit value in KB"
+            value={
+              limitValue !== null && limitValue !== -1
+                ? limitValue.toString()
+                : ""
+            }
+            onChange={(e) => setLimitValue(Number(e.target.value))}
+          />
           <button
-            onClick={handleDeleteLimit}
-            className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
+            onClick={handleSetLimit}
+            className={`px-4 p-2 rounded-r-lg text-white border border-r-0 ${
+              limitValue === initialLimit
+                ? "bg-blue-200 cursor-not-allowed"
+                : "bg-blue-500 hover:bg-blue-400"
+            }`}
+            disabled={limitValue === initialLimit} // Disabilita il pulsante se i limiti sono uguali
           >
-            Delete Limit
+            Set Limit
           </button>
-        )}
+        </div>
+        <button
+          onClick={handleDeleteLimit}
+          className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
+        >
+          Delete Limit
+        </button>
         <button
           onClick={() => navigate(-1)} // Torna alla home senza refresh
           className="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded ml-2"
