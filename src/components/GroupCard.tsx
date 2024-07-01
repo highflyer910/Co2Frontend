bbf2d132d7fa8b5fd46448fe6e7d8429b36f9935
boobaGreen <devboobagreen@gmail.com>
2024-07-01 03:03:24 +0200

    group accordion 10

--- src/components/GroupCard.tsx
+++ src/components/GroupCard.tsx
@@ -24,7 +24,7 @@ const GroupCard: React.FC<GroupCardProps> = ({
   const [selectedDonationId, setSelectedDonationId] = useState<string | null>(
     null
   );
-  const [isExpanded, setIsExpanded] = useState(false); // Stato per gestire l'espansione
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -61,27 +61,32 @@ const GroupCard: React.FC<GroupCardProps> = ({
 
   return (
     <div className="mt-8 relative w-full max-w-xs bg-white text-green-800 font-body py-2 px-4 rounded border-2 border-green-800 shadow-md hover:bg-gray-100 flex flex-col items-start">
+      {/* Prima riga */}
       <div className="flex items-center justify-between w-full">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center">
           <h2 className="font-bold text-xl">{group.groupName}</h2>
-          <button
-            onClick={() => toggleFavourite(group.groupId)}
-            className="ml-2 text-2xl focus:outline-none"
-          >
-            {isFavourite ? (
-              <AiFillStar className="text-green-500" />
-            ) : (
-              <AiOutlineStar className="text-gray-500" />
-            )}
-          </button>
         </div>
+        <button
+          onClick={() => toggleFavourite(group.groupId)}
+          className="text-2xl focus:outline-none"
+        >
+          {isFavourite ? (
+            <AiFillStar className="text-green-500" />
+          ) : (
+            <AiOutlineStar className="text-gray-500" />
+          )}
+        </button>
       </div>
+
+      {/* Seconda riga */}
       <button
         onClick={toggleExpand}
         className="w-full mt-2 mb-4 bg-yellow-400 hover:bg-yellow-500 text-green-900 font-bold py-2 px-4 rounded"
       >
         {isExpanded ? "Hide Details" : "Show Details"}
       </button>
+
+      {/* Contenuto sempre visibile */}
       <div className="m-2">
         <p>Limits (KB) : {limitToShow}</p>
         <p>
@@ -98,6 +103,8 @@ const GroupCard: React.FC<GroupCardProps> = ({
           ))}
         </p>
       </div>
+
+      {/* Contenuto espandibile */}
       {isExpanded && (
         <div className="m-2">
           <p>Total Messages : {group.totalMessages}</p>
@@ -145,7 +152,9 @@ const GroupCard: React.FC<GroupCardProps> = ({
           <p>Sticker Emissions SWD (g): {group.stickerEmissionsSWDMethod}</p>
         </div>
       )}
-      <div className="flex flex-row justify-between w-full mt-auto">
+
+      {/* Pulsanti in fondo */}
+      <div className="flex justify-between w-full mt-auto">
         <button
           onClick={() => navigateToDonatePage(group.groupId, group.groupName)}
           className="my-4 bg-yellow-400 hover:bg-yellow-500 text-green-900 font-bold py-2 px-4 rounded"
@@ -170,6 +179,8 @@ const GroupCard: React.FC<GroupCardProps> = ({
           </button>
         )}
       </div>
+
+      {/* Modal per le donazioni */}
       {selectedDonationId && (
         <DonationModal
           donationId={selectedDonationId}
