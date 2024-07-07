8c4bb2953b21cb748ddebd881ef09e59cfed72ad
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-07-07 20:10:46 +0400

    updated

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,17 +1,9 @@
 // LandingPage.tsx
 import React from "react";
-// import { useNavigate } from "react-router-dom";
 import TelegramLoginButton from "../elements/TelegramLoginButton";
 import Header from "../components/Header";
 
 const LandingPage: React.FC = () => {
-  // const navigate = useNavigate();
-
-  // if (jwt) {
-  //   console.log("User is authenticated, redirecting to dashboard");
-  //   navigate("/groups");
-  // }
-
   return (
     <div className="relative bg-gray-100 min-h-screen">
       <img
@@ -28,15 +20,31 @@ const LandingPage: React.FC = () => {
         className="relative flex flex-col items-center justify-center py-16 md:pt-0 pt-0"
         role="main"
       >
-        <h1 className="font-poppins text-3xl font-bold text-center bg-green-700 text-yellow-200 py-3 px-4 shadow-lg">
-          Welcome to GoGreen
-        </h1>
+        <div className="w-full bg-green-700 text-yellow-200 py-3 px-4 shadow-lg">
+          <h1 className="font-poppins text-3xl font-bold text-center">
+            Welcome to GoGreen
+          </h1>
+        </div>
 
-        <p className="font-body text-2xl text-shadow text-bold text-green-950 mb-6 mt-4 text-center">
-          Start making a difference today!
-        </p>
-
-        <TelegramLoginButton />
+        <div className="flex flex-col items-center mt-8">
+          <div className="relative flex items-center">
+            <img
+              src="/tree.png"
+              alt="Tree"
+              className="w-32 h-32 md:w-48 md:h-48"
+            />
+            <div className="-ml-2 md:-ml-4 w-48 md:w-64">
+              <div className="relative flex justify-center items-center border-4 rounded-lg border-green-700 p-2 shadow-lg">
+                <p className="font-body text-lg md:text-xl text-shadow text-bold text-green-900 text-center">
+                  Start making a difference today!
+                </p>
+                {/* Adjusted arrow position */}
+                <div className="absolute top-1/4 -left-2 w-0 h-0 border-t-8 border-b-8 border-r-8 border-t-transparent border-b-transparent border-r-green-700"></div>
+              </div>
+            </div>
+          </div>
+          <TelegramLoginButton />
+        </div>
       </main>
     </div>
   );
