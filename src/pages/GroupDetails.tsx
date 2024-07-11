e74d80c2212e609c0647071f958d87135ce62547
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-07-11 18:02:07 +0400

    Update GroupDetails.tsx

--- src/pages/GroupDetails.tsx
+++ src/pages/GroupDetails.tsx
@@ -72,21 +72,21 @@ const GroupDetails: React.FC = () => {
 
       <main className="relative flex flex-col items-center justify-center md:pt-0 pt-0 pb-16">
         <div className="w-full bg-green-700 text-yellow-200 py-3 px-4 shadow-lg">
-          <h1 className="font-poppins text-3xl font-bold text-center w-full">
+          <h1 className="font-poppins text-2xl md:text-3xl font-bold text-center w-full">
             GoGreen CardGroups
           </h1>
         </div>
 
         <div className="flex items-center mt-4">
           <img src="/tree.png" alt="Tree" className="w-12 h-12 mr-2 md:w-24 md:h-24 md:mr-2" />
-          <h2 className="font-poppins text-3xl font-bold text-green-800">
+          <h2 className="font-poppins text-2xl md:text-3xl font-bold text-green-800">
             {group.groupName}
           </h2>
         </div>
 
-        <div className="mt-8 bg-yellow-200 text-green-700 font-body py-4 px-4 rounded-2xl border-2 border-green-800 shadow-md w-full max-w-md relative">
+        <div className="mt-8 bg-yellow-200 text-green-700 font-body py-4 px-4 md:px-6 rounded-2xl border-2 border-green-800 shadow-md w-full max-w-md relative mx-4 md:mx-0">
           <button 
-            className="absolute top-4 right-4 bg-green-700 hover:bg-green-800 text-yellow-200 font-bold py-2 px-4 rounded-lg"
+            className="absolute top-2 right-2 bg-green-700 hover:bg-green-800 text-yellow-200 font-bold py-2 px-4 rounded-lg"
             onClick={() => setShowAllDetails(!showAllDetails)}
           >
             {showAllDetails ? "See Less" : "See More"}
