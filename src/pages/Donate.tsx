0b1e3a6717e10d03814d92d5d12cb991102e11c6
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-07-11 23:44:40 +0400

    Update Donate.tsx

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -47,7 +47,10 @@ const Donate: React.FC = () => {
         aria-hidden="true"
       />
       <Header />
-      <main className="flex-grow flex flex-col items-center justify-center px-4">
+      <main
+        className="relative flex flex-col items-center justify-center py-16 md:pt-0 pt-0"
+        role="main"
+      >
       <div className="w-full bg-green-700 text-yellow-200 py-3 px-4 shadow-lg">
           <h1 className="font-poppins text-3xl font-bold text-center">
             Welcome to GoGreen
