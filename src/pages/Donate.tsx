023999d86071234211931bbd00cd436ffbb85909
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-07-12 19:26:21 +0400

    Update Donate.tsx

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -92,13 +92,13 @@ const Donate: React.FC = () => {
             onClick={handleDonateClick}
             className="bg-yellow-400 hover:bg-yellow-500 text-green-900 font-bold py-2 px-4 rounded"
           >
-            Donate
+            Plant!
           </button>
           <button
             onClick={handleCancelClick}
             className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
           >
-            Cancel
+            Go Back
           </button>
         </div>
       </main>
