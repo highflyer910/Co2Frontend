33a8923e141a07942f282d753400179ea3cf3e57
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-07-12 19:21:42 +0400

    Update Donate.tsx

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -51,20 +51,29 @@ const Donate: React.FC = () => {
         className="relative flex flex-col items-center justify-center py-16 md:pt-0 pt-0"
         role="main"
       >
-      <div className="w-full bg-green-700 text-yellow-200 py-3 px-4 shadow-lg">
+        <div className="w-full bg-green-700 text-yellow-200 py-3 px-4 shadow-lg">
           <h1 className="font-poppins text-3xl font-bold text-center">
             Let's GoGreen!
           </h1>
         </div>
-        <div className="relative mb-8">
-          <img src="/tree.png" alt="Tree" className="w-32 h-32" />
-          <div className="absolute top-0 left-full ml-4 bg-yellow-200 text-green-800 p-3 rounded-lg shadow-md max-w-xs">
-            <p className="text-sm">
-              Help {groupName} grow their forest! Donate!
-            </p>
+        <div className="flex flex-col items-center mt-8">
+          <div className="relative flex items-center">
+            <img
+              src="/tree.png"
+              alt="Tree"
+              className="w-32 h-32 md:w-48 md:h-48"
+            />
+            <div className="-ml-2 md:-ml-4 w-48 md:w-64">
+              <div className="relative flex justify-center items-center border-4 rounded-lg border-green-700 p-2 shadow-lg bg-yellow-200">
+                <p className="font-body text-lg md:text-xl text-shadow text-bold text-green-900 text-center">
+                  Help {groupName} grow their forest! Donate!
+                </p>
+                <div className="absolute top-1/4 -left-2 w-0 h-0 border-t-8 border-b-8 border-r-8 border-t-transparent border-b-transparent border-r-green-700"></div>
+              </div>
+            </div>
           </div>
         </div>
-        <h2 className="text-2xl font-bold text-green-800 mb-4">
+        <h2 className="text-2xl font-bold text-green-800 mb-4 mt-8">
           Choose trees quantity
         </h2>
         <img src="/tree1.png" alt="Tree" className="w-24 h-16 mb-4" />
