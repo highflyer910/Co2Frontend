b5585f7c424e93235590f6ed6ea7c5e70b77cd7e
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-07-13 04:48:20 +0400

    Update Donate.tsx

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -63,7 +63,7 @@ const Donate: React.FC = () => {
         </div>
         <div className="flex flex-col items-center mt-8 mb-12 px-4 w-full">
           <div className="relative flex items-center justify-between w-full max-w-xl">
-            <div className="flex items-center mr-8">
+            <div className="flex items-center mr-4">
               <img
                 src="/tree.png"
                 alt="Tree"
