28e9b2f869ae4255f60acf94540d950288af1559
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-07-14 01:52:48 +0400

    Update Groups.tsx

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -103,7 +103,7 @@ const Groups: React.FC = () => {
               />
               <button
                 onClick={() => setShowVideo(true)}
-                className="text-green-700 text-3xl focus:outline-none"
+                className="text-green-700 text-5xl focus:outline-none"
                 aria-label="More options"
               >
                 &#8230;
