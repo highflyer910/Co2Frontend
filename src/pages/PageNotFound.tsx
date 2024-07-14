131c81b423490fede8f751706adc76d2fc900f2c
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-07-14 19:30:10 +0400

    Update PageNotFound.tsx

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -14,16 +14,25 @@ const PageNotFound: React.FC = () => {
       
       <Header />
 
-      <div className="relative z-10 flex flex-col items-center justify-center">
+      <div className="w-full bg-green-700 text-yellow-200 py-3 px-4 shadow-lg text-center z-10">
+        <h1 className="font-poppins text-3xl font-bold">
+          Welcome To GoGreen
+        </h1>
+      </div>
+
+      <div className="relative z-10 flex flex-col items-center justify-center mt-8">
         <div className="text-center mb-4">
           <img src="/fail.png" alt="Fail" className="w-36 h-36 mx-auto mb-4" />
           <div className="relative inline-block">
-            <div className="absolute -top-10 -left-10 bg-yellow-200 border border-green-700 rounded-lg p-4 shadow-lg">
+            <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 bg-yellow-200 border border-green-700 rounded-lg p-4 shadow-lg">
               <p className="text-xl font-bold text-green-900">Ops! Page not found!</p>
             </div>
           </div>
         </div>
-        <Link to="/" className="mt-4 bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded">
+        <Link 
+          to="/" 
+          className="mt-4 bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded"
+        >
           Go back to Home!
         </Link>
       </div>
