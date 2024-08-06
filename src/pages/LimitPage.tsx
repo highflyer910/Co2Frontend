4b43d42b1596b0f7a90240ec8d54566f2f0be32f
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-08-06 19:33:42 +0400

    added header

--- src/pages/LimitPage.tsx
+++ src/pages/LimitPage.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { useGetGroups } from "../hooks/useGetAllGroups"; // Assumi che il percorso sia corretto
+import { useGetGroups } from "../hooks/useGetAllGroups";
 import { useMain } from "../contexts/MainContext";
+import Header from "../components/Header";
 
 const Limit: React.FC = () => {
   const { groupId, groupName, groupLimits } = useParams<{
@@ -11,15 +12,13 @@ const Limit: React.FC = () => {
   }>();
 
   const navigate = useNavigate();
-  const { jwt } = useMain(); // Ottieni il token JWT
-  const { refetch } = useGetGroups(jwt); // Ottieni refetch dal custom hook
+  const { jwt } = useMain();
+  const { refetch } = useGetGroups(jwt);
 
-  // State per gestire il valore del limite in KB
   const [limitValue, setLimitValue] = useState<number | null>(
     groupLimits && groupLimits !== "-1" ? +groupLimits : -1
   );
 
-  // State per gestire i messaggi di risposta
   const [responseMessage, setResponseMessage] = useState<string | null>(null);
 
   const handleSetLimit = async () => {
@@ -34,22 +33,22 @@ const Limit: React.FC = () => {
           },
           body: JSON.stringify({
             chatId: groupId,
-            limit: limitValue, // Assumiamo che il valore sia già in KB
+            limit: limitValue,
           }),
         }
       );
       const data = await response.json();
-      setResponseMessage(data.success); // Mostra il messaggio di successo
+      setResponseMessage(data.success);
 
       if (data.success) {
         console.log("refetch");
-        await refetch(); // Effettua il refetch dei gruppi
+        await refetch();
       }
 
-      setLimitValue(limitValue); // Aggiorna il valore del limite
+      setLimitValue(limitValue);
     } catch (error) {
       console.error("Error setting limit:", error);
-      setResponseMessage("Error setting limit"); // Gestione dell'errore
+      setResponseMessage("Error setting limit");
     }
   };
 
@@ -68,83 +67,96 @@ const Limit: React.FC = () => {
 
       let data;
       if (response.status === 204) {
-        data = { status: "success", message: "Limit deleted successfully" }; // Genera una risposta fittizia
+        data = { status: "success", message: "Limit deleted successfully" };
       } else {
         data = await response.json();
       }
 
       if (data.status === "success") {
-        setResponseMessage(data.message); // Mostra il messaggio di successo
-        await refetch(); // Effettua il refetch dei gruppi
+        setResponseMessage(data.message);
+        await refetch();
         console.log("refetch");
-        setLimitValue(-1); // Resetta il valore del limite a -1 (valore di default)
+        setLimitValue(-1);
       } else {
-        setResponseMessage("Error deleting limit"); // Gestione dell'errore
+        setResponseMessage("Error deleting limit");
       }
     } catch (error) {
       console.error("Error deleting limit:", error);
-      setResponseMessage("Error deleting limit"); // Gestione dell'errore
+      setResponseMessage("Error deleting limit");
     }
   };
 
   useEffect(() => {
-    // Esempio di utilizzo dei parametri
     console.log(`Group ID: ${groupId}, Group Name: ${groupName}`);
   }, [groupId, groupName]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="font-poppins text-3xl font-bold text-center bg-yellow-200 text-green-800 py-3 px-4 shadow-lg">
-        Limit Group Name: {groupName}
-      </h1>
-      <div className="my-4">
-        <div className="flex items-center mb-4">
-          <input
-            type="number"
-            className="rounded-l-lg p-2 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
-            placeholder="Enter limit value in KB"
-            value={
-              limitValue !== null && limitValue !== -1
-                ? limitValue.toString()
-                : ""
-            }
-            onChange={(e) => setLimitValue(Number(e.target.value))}
-          />
+    <div className="relative bg-gray-100 flex flex-col min-h-screen overflow-auto">
+      <img
+        src={`/background.webp`}
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover z-0"
+        aria-hidden="true"
+      />
+
+      <Header />
+
+      <main className="relative flex flex-col items-center justify-center md:pt-0 pt-0">
+        <div className="w-full bg-green-700 text-yellow-200 py-3 px-4 shadow-lg">
+          <h1 className="font-poppins text-3xl font-bold text-center">
+            Limit Group Name: {groupName}
+          </h1>
+        </div>
+
+        <div className="my-4">
+          <div className="flex items-center mb-4">
+            <input
+              type="number"
+              className="rounded-l-lg p-2 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
+              placeholder="Enter limit value in KB"
+              value={
+                limitValue !== null && limitValue !== -1
+                  ? limitValue.toString()
+                  : ""
+              }
+              onChange={(e) => setLimitValue(Number(e.target.value))}
+            />
+            <button
+              onClick={handleSetLimit}
+              className={`px-4 p-2 rounded-r-lg text-white border border-r-0  bg-blue-500 hover:bg-blue-400`}
+            >
+              Set Limit
+            </button>
+          </div>
           <button
-            onClick={handleSetLimit}
-            className={`px-4 p-2 rounded-r-lg text-white border border-r-0  bg-blue-500 hover:bg-blue-400`}
+            onClick={handleDeleteLimit}
+            className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
           >
-            Set Limit
+            Delete Limit
+          </button>
+          <button
+            onClick={() => navigate(-1)}
+            className="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded ml-2"
+          >
+            Go Back
           </button>
         </div>
-        <button
-          onClick={handleDeleteLimit}
-          className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
-        >
-          Delete Limit
-        </button>
-        <button
-          onClick={() => navigate(-1)} // Torna alla home senza refresh
-          className="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded ml-2"
-        >
-          Torna Alla Home
-        </button>
-      </div>
-      {responseMessage && (
-        <div
-          className={`my-4 text-center font-bold ${
-            responseMessage.includes("Error")
-              ? "text-red-600"
-              : "text-green-600"
-          }`}
-        >
-          {responseMessage}
-        </div>
-      )}
+        {responseMessage && (
+          <div
+            className={`my-4 text-center font-bold ${
+              responseMessage.includes("Error")
+                ? "text-red-600"
+                : "text-green-600"
+            }`}
+          >
+            {responseMessage}
+          </div>
+        )}
 
-      <p className="text-center text-gray-600 mt-2">
-        Please enter the limit value in kilobytes (KB).
-      </p>
+        <p className="text-center text-gray-600 mt-2">
+          Please enter the limit value in kilobytes (KB).
+        </p>
+      </main>
     </div>
   );
 };
